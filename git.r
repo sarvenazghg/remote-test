f1f3f29c14d861b48f590279bcb51a2116899c8d
sarvenazghg <sarvenaz.ghafarigarakani@mail.mcgill.ca>
2021-10-20 10:22:20 -0400

    I added some line to the main

--- git.r
+++ git.r
@@ -3,4 +3,6 @@ library(gitcreds)
 gitcreds_get()
 gitcreds_set()    
 2
-# I could set up a new token , so I dont need the PW anymore
+# I could set up a new token , so I don't need the PW anymore.
+# ignore xlsx is working as a safety measure ! 
+# so it doesnt make any changes to the original
